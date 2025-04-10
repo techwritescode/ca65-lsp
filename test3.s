29ed04e8c6ac0d2a54a6f681c6e53aba46bc7549
tech <10032418+techwritescode@users.noreply.github.com>
2025-04-10 13:06:49 -0500

    Fix crash during goto definition

--- test3.s
+++ test3.s
@@ -1,4 +1,5 @@
 MY_CONST = $10
+ScreenEdge_X_Pos = $20
 
 .proc test_proc
 
@@ -12,5 +13,6 @@ main:
     jmp main
     test 1, 2, 3
     jmp test_proc
+    jmp t
     lda #MY_CONST
     lda #ScreenEdge_X_Pos
