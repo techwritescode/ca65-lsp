7222a90598eabc6394224b0c2c0f3e45c96851ee
Simon Hochrein <10032418+simonhochrein@users.noreply.github.com>
2025-04-10 13:04:06 -0500

    Organization + LSP improvements

---
+++ lsp/src/completion.rs
@@ -0,0 +1,109 @@
+use crate::asm_server::State;
+use crate::codespan::FileId;
+use crate::instructions;
+use crate::symbol_cache::{symbol_cache_get, SymbolType};
+use codespan::Position;
+use tower_lsp_server::lsp_types::{CompletionItem, CompletionItemKind, CompletionItemLabelDetails, InsertTextFormat};
+
+static BLOCK_CONTROL_COMMANDS: &[&str] = &[
+    "scope", "proc", "macro", "enum", "union", "if", "repeat", "struct",
+];
+
+pub trait CompletionProvider {
+    fn completions_for(&self, state: &State, id: FileId, position: Position)
+        -> Vec<CompletionItem>;
+}
+
+pub struct InstructionCompletionProvider;
+
+impl CompletionProvider for InstructionCompletionProvider {
+    fn completions_for(
+        &self,
+        state: &State,
+        id: FileId,
+        position: Position,
+    ) -> Vec<CompletionItem> {
+        if state.files.show_instructions(id, position) {
+            instructions::INSTRUCTION_MAP
+                .get()
+                .expect("Instructions not loaded")
+                .iter()
+                .map(|(opcode, description)| CompletionItem {
+                    label: opcode.to_lowercase().to_owned(),
+                    detail: Some(description.to_owned()),
+                    kind: Some(CompletionItemKind::KEYWORD),
+                    ..Default::default()
+                })
+                .collect()
+        } else {
+            Vec::new()
+        }
+    }
+}
+
+pub struct SymbolCompletionProvider;
+
+impl CompletionProvider for SymbolCompletionProvider {
+    fn completions_for(
+        &self,
+        state: &State,
+        id: FileId,
+        position: Position,
+    ) -> Vec<CompletionItem> {
+        let show_instructions = state.files.show_instructions(id, position); // Makes a naive guess at whether the current line contains an instruction. Doesn't work on lines with labels
+
+        symbol_cache_get()
+            .iter()
+            .filter_map(|symbol| {
+                if show_instructions
+                    && matches!(symbol.sym_type, SymbolType::Label | SymbolType::Constant)
+                {
+                    None
+                } else if !show_instructions && matches!(symbol.sym_type, SymbolType::Macro) {
+                    None
+                } else {
+                    Some(CompletionItem {
+                        label: symbol.label.to_owned(),
+                        detail: Some(symbol.comment.to_owned()),
+                        label_details: Some(CompletionItemLabelDetails{
+                            detail: None,
+                            description: state.files.get_uri_relative(symbol.file_id, id),
+                        }),
+                        kind: Some(match symbol.sym_type {
+                            SymbolType::Label => CompletionItemKind::FUNCTION,
+                            SymbolType::Constant => CompletionItemKind::CONSTANT,
+                            SymbolType::Macro => CompletionItemKind::SNIPPET,
+                        }),
+                        ..Default::default()
+                    })
+                }
+            })
+            .collect()
+    }
+}
+
+pub struct BlockControlCompletionProvider;
+
+impl CompletionProvider for BlockControlCompletionProvider {
+    fn completions_for(
+        &self,
+        state: &State,
+        id: FileId,
+        position: Position,
+    ) -> Vec<CompletionItem> {
+        if state.files.show_instructions(id, position) {
+            BLOCK_CONTROL_COMMANDS
+                .iter()
+                .map(|command| CompletionItem {
+                    label: (*command).to_string(),
+                    kind: Some(CompletionItemKind::FUNCTION),
+                    insert_text: Some(format!(".{} $1\n\t$0\n.end{} ; End $1", *command, *command)),
+                    insert_text_format: Some(InsertTextFormat::SNIPPET),
+                    ..Default::default()
+                })
+                .collect()
+        } else {
+            Vec::new()
+        }
+    }
+}
---
+++ lsp/src/definition.rs
@@ -0,0 +1,31 @@
+use crate::codespan::{FileId, Files};
+use crate::symbol_cache::{symbol_cache_fetch, Symbol};
+use codespan::{FileError, Position};
+use std::cmp::Ordering;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Definition;
+
+impl Definition {
+    pub fn get_definition_position(
+        &self,
+        files: &Files,
+        id: FileId,
+        position: Position,
+    ) -> Result<Option<Vec<Symbol>>, FileError> {
+        let word = files
+            .get(id)
+            .get_word_at_position(position)?;
+
+        let mut definitions = symbol_cache_fetch(word.to_string());
+
+        definitions.sort_by(|sym, _| {
+            if sym.file_id == id {
+                return Ordering::Less;
+            }
+            Ordering::Equal
+        });
+        
+        Ok(Some(definitions))
+    }
+}
