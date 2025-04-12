8007a1cf863b5618c87afd47c853de8b9b88dda1
Taylor Plewe <taylor.plewe@awardco.com>
2025-04-12 11:57:54 -0600

    copy some changes from my other branch, add simple completion items for every keyword

--- lsp/src/ca65_doc.rs
+++ lsp/src/ca65_doc.rs
@@ -4,30 +4,36 @@ use std::{
 };
 use serde::Deserialize;
 
+type Key = String;
+type Documentation = String;
+
 #[derive(Deserialize)]
-pub struct Ca65Doc {
-    keywords_to_markdown: HashMap<String, String>,
-    aliases: HashMap<String, String>,
+pub struct IndexedDocumentation {
+    keys_to_doc: HashMap<Key, Documentation>,
+    keys_with_shared_doc: HashMap<Key, Key>,
 }
 
-impl Ca65Doc {
+impl IndexedDocumentation {
     pub fn get_doc_for_word(&self, word: &str) -> Option<String> {
-        if let Some(doc) = self.keywords_to_markdown.get(word) {
+        if let Some(doc) = self.keys_to_doc.get(word) {
             Some(doc.clone())
-        } else if let Some(alias) = self.aliases.get(word) {
-            Some(self.keywords_to_markdown.get(alias).expect("ca65 doc alias does not match a keyword").clone())
+        } else if let Some(alias) = self.keys_with_shared_doc.get(word) {
+            Some(self.keys_to_doc.get(alias).expect("ca65 doc alias does not match a keyword").clone())
         } else {
             None
         }
     }
+    pub fn get_vec_of_all_entries(&self) -> Vec<(String, String)> {
+        self.keys_to_doc.iter().map(|(k, v)| (k.clone(), v.clone())).collect()
+    }
 }
 
-pub static CA65_DOC: OnceLock<Ca65Doc> = OnceLock::new();
+pub static CA65_DOC: OnceLock<IndexedDocumentation> = OnceLock::new();
 
 pub fn parse_json_to_hashmap() {
-    if let Ok(doc) = serde_json::from_str::<Ca65Doc>(include_str!("../../data/ca65-keyword-doc.json")) {
+    if let Ok(doc) = serde_json::from_str::<IndexedDocumentation>(include_str!("../../data/ca65-keyword-doc.json")) {
         if CA65_DOC.set(doc).is_err() {
-            eprintln!("CA65_KEYWORDS_MAP not able to be initialized");
+            eprintln!("CA65_DOC not able to be initialized");
         }
     }
 }
