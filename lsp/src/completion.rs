506cca62822e73f85c0194fd78be4be76d8aaf18
Taylor Plewe <taylor.plewe@awardco.com>
2025-05-06 21:50:44 -0600

    include tokens which end on the same position the line ends on

--- lsp/src/completion.rs
+++ lsp/src/completion.rs
@@ -5,6 +5,7 @@ use crate::symbol_cache::{symbol_cache_get, SymbolType};
 use crate::documentation::{CA65_KEYWORD_COMPLETION_ITEMS, FEATURE_COMPLETION_ITEMS, MACPACK_COMPLETION_ITEMS, INSTRUCTION_COMPLETION_ITEMS};
 use codespan::Position;
 use tower_lsp_server::lsp_types::{CompletionItem, CompletionItemKind, CompletionItemLabelDetails};
+use parser::TokenType;
 
 pub trait CompletionProvider {
     fn completions_for(&self, state: &State, id: FileId, position: Position)
@@ -91,7 +92,12 @@ impl CompletionProvider for MacpackCompletionProvider {
         id: FileId,
         position: Position
     ) -> Vec<CompletionItem> {
-        if state.files.line_tokens(id, position).last().is_some_and(|tok| tok.lexeme == ".macpack") {
+        if state.files.line_tokens(id, position)
+            .iter()
+            .filter(|tok| tok.token_type != TokenType::EOL)
+            .nth_back(1)
+            .is_some_and(|tok| tok.lexeme == ".macpack"
+            ) {
             MACPACK_COMPLETION_ITEMS.get().expect("Could not get MACPACK_COMPLETION_ITEMS in completion provider").clone()
         } else {
             Vec::new()
@@ -107,7 +113,12 @@ impl CompletionProvider for FeatureCompletionProvider {
         id: FileId,
         position: Position
     ) -> Vec<CompletionItem> {
-        if state.files.line_tokens(id, position).last().is_some_and(|tok| tok.lexeme == ".feature") {
+        if state.files.line_tokens(id, position)
+            .iter()
+            .filter(|tok| tok.token_type != TokenType::EOL)
+            .nth_back(1)
+            .is_some_and(|tok| tok.lexeme == ".feature"
+        ) {
             FEATURE_COMPLETION_ITEMS.get().expect("Could not get FEATURE_COMPLETION_ITEMS in completion provider").clone()
         } else {
             Vec::new()
