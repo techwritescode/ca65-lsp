6781cd02cd272994dd9c1eaa201cb79dfcbe9b7b
techwritescode <10032418+techwritescode@users.noreply.github.com>
2025-08-10 11:36:47 -0500

    Fix inconsistent paths

--- lsp/src/data/convert_uri.rs
+++ lsp/src/data/convert_uri.rs
@@ -1,7 +1,9 @@
+use std::fmt::format;
 use std::str::FromStr;
 use anyhow::anyhow;
 use tower_lsp_server::lsp_types::Uri;
 use urlencoding::encode;
+use crate::state::{URI_MODE, UriMode};
 
 #[cfg(target_os = "windows")]
 pub fn convert_uri(uri: Uri) -> anyhow::Result<Uri> {
@@ -12,8 +14,15 @@ pub fn convert_uri(uri: Uri) -> anyhow::Result<Uri> {
 
     let drive_letter = segments[0].chars().next().unwrap();
 
-    let drive = format!("{}:", drive_letter.to_ascii_lowercase());
-    segments[0] = encode(&drive).to_string();
+    segments[0] = match *URI_MODE.lock().unwrap() {
+        UriMode::VSCode => {
+            let drive = format!("{}:", drive_letter.to_ascii_lowercase());
+            encode(&drive).to_string()
+        }
+        UriMode::Helix => {
+            format!("{}:", drive_letter.to_ascii_uppercase())
+        }
+    };
 
     let path = format!("file:///{}", segments.join("/"));
     Uri::from_str(&path).map_err(|e| anyhow!(e))
